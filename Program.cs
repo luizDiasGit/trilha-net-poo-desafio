3dc85968da7d98deb131e4370ff2984de832012a
Luiz Eduardo Dias <luiz.dias602120@gmail.com>
2024-01-02 22:37:17 -0300

    Desafio POO Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia:");
+Smartphone Nokia = new Nokia(numero: "1234567",modelo: "Modelo 1",imei: "111111111", memoria: 64);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("Clash Royale");
+
+
+Console.WriteLine("\nIphone:");
+Smartphone Iphone = new Iphone(numero: "1234567",modelo: "Modelo 1",imei: "111111111", memoria: 64);
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Netflix");
